9d1574f9e0ced6e32b9609739cd5c0a9770b9a2d
SuperChipper <1625703948@qq.com>
2022-07-10 22:49:24 -0700

    test

---
+++ build/CMakeFiles/cv_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_test.dir/Detect.cpp.o"
+  "CMakeFiles/cv_test.dir/main.cpp.o"
+  "CMakeFiles/cv_test.dir/video_test.cpp.o"
+  "cv_test"
+  "cv_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
